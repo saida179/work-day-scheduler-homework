8d57278e507fbbe7183555de6b73e56b15d03d25
saida179 <69127945+saida179@users.noreply.github.com>
2020-09-16 23:53:43 -0700

    getting ready to turn in homework

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ $(document).ready(function(){
 //WHEN I view the time blocks for that day
 //THEN each time block is color coded to indicate 
 //whether it is in the past, present, or future
+let timeColors= document.querySelectorAll([".past", ".present", ".future"]);
 let timeSlotIds = document.querySelectorAll(["text-9am", "text-10am", "text-11am", "text-12pm", "text-1pm", "text-2pm", "text-3pm", "text-4pm", "text-5pm", "text-6pm" ]);
+$(document).ready(function(){
+
+})
 
 
 //WHEN I click the save button for that time block
 $(".fas fa-save").on("click"), function () {
-    console.log("button works");  
+    console.log(".fas fa-save");  
 };
